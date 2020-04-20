8aacdba9ae9c8c966f2fff675462bbdc2e10282f
cjy <jy_tajent@yahoo.com.sg>
2020-04-21 00:03:03 +0800

    edit for flow map and filter aggregate flow map link to filter aggregate filter aggregate solved "to" limit to available ones only

--- ProjRShiny_jiayi/server.R
+++ ProjRShiny_jiayi/server.R
@@ -1,81 +1,349 @@
 ######################### Define SERVER logic ----##################
 
-server <- function(input, output) {
+server <- function(input, output, session) {
     ############################################## Jia Yi ####################################################
     ## Num of commuters in Jan 2020 between any 2 busstops
-    # Remove the matrix diagonal
-    diag(myflows) <- 0
     
-    # Selection of flows > 500
-    flowSel1 <- firstflowsg(mat = myflows, method = "xfirst", k = 5000)
-    # Selection of flows > 1000
-    flowSel2 <- firstflowsg(mat = myflows, method = "xfirst", k = 10000)
-    # table output where flowSel1 ( k <=5000))
-    flowtable1 <- compmat(mat1 = myflows, mat2 = myflows * flowSel1, digits = 1)
-    # table output where flowSel1 ( k <=10000))
-    flowstable2 <- compmat(mat1 = myflows, mat2 = myflows * flowSel2, digits = 1)
+    ### Planning Area Dropdown
     
-    output$flow_jy <- renderPrint({ 
-        {if(input$flow_input== 5000){flowtable1
-        }else if(input$flow_input == 10000){flowstable2
+    # output$ex_out <- renderPrint({
+    #     str(sapply(sprintf('e%d', 0:7), function(id) {
+    #         input[[id]]
+    #     }, simplify = FALSE))
+    # })
+    ###################################################### For Aggregate_Filter Tab JY ######################################################
+    # Show PA options when 
+    # datasetInput <- reactive({
+    #     switch(input$radio,
+    #            "Planning Area" = 'PA')
+    # })
+    # read in data (this data will change so we put in server)
+    node_SZ <- read_csv("data/node_flowmap.csv")
+    edge_id2_SZ <- read_csv("data/edge_flowmap.csv")
+    
+    output$ex_out <- reactive({
+        str(c(input$radio,input$pa_from, input$pa_to, input$sz_from, input$sz_to)
+        )
+    })
+    
+    outputOptions(output, "ex_out", suspendWhenHidden = FALSE)      
+    
+  
+    
+    # plot the FROM Table
+    output$mytableFrom = DT::renderDataTable({
+        if (input$radio=="PA"){
+            busstops%>% filter(busstops$planning_area %in% input$pa_from)
         }
+        else{
+            busstops%>% filter(busstops$subzone_name %in% input$sz_from)
         }
-    }) # end of renderPrint Statistics
+    }) # end of renderDataTable FROM
+    # https://community.rstudio.com/t/evaluation-error-operation-not-allowed-without-an-active-reactive-context/18468/3
     
-    # Plot Flows diagram
-    ## Remove the matrix diagonal
-    diag(myflows) <- 0
+    # # make sure the To table has available locations the From pick can go to
+    # availableTo <- reactive({
+    #   if (input$radio=="PA"){
+    #     ## KIV
+    #     busstops%>% filter(busstops$planning_area %in% input$pa_to)
+    #   }
+    #   else{
+    #     busstops <- busstops%>% filter(busstops$subzone_name %in% node_SZ)
+    #   }
+    # }) #End of reactive
+    # reference
+    # edge_id2_SZ <- edge_id2_SZ[which(edge_id2_SZ$from %in% node2$id) ,] 
     
-    ## Select flows that represent at least 20% of the sum of outgoing flows for 
-    ## each urban area. ( can select other methods )
-    flowSel1 <- firstflows(mat = myflows/rowSums(myflows)*100, method = "xfirst", 
-                           k = 20)
+    # plot the TO Table
+    output$mytableTo = DT::renderDataTable({
+        if (input$radio=="PA"){
+          ## KIV
+            busstops%>% filter(busstops$planning_area %in% input$pa_to)
+        }
+        else{
+            busstops%>% filter(busstops$subzone_name %in% input$sz_to)
+        }
+    }) # end of renderDataTable TO
+    
+    ### Very Important: To update the "To" list based on edges from the "From"
+    sz_selection_from <- reactive({
+      nodeidFrom <- node_SZ[which(node_SZ$name %in% input$sz_from) ,] 
+      edgeid <- edge_id2_SZ[which(edge_id2_SZ$from %in% nodeidFrom$id),] 
+      nodeidTo <- node_SZ[which(node_SZ$id %in% edgeid$to) ,]
+      nodeNameTo <- unique(nodeidTo$name)
+      nodeNameTo #grab the last variable
+    })
+    
+    # https://shiny.rstudio.com/articles/selectize.html
+    observe({
+      updateSelectizeInput(session, 'sz_to', choices=sz_selection_from(), server=TRUE)
+    })
+    ###
+    
+  
+    
+    # Download table "from"
+    output$downloadFrom <- downloadHandler(
+        filename = function(){"source.csv"}, 
+        content = function(fname){
+            write.csv(
+                if (input$radio=="PA"){
+                    busstops%>% filter(busstops$planning_area %in% input$pa_from)
+                }
+                else{
+                    busstops%>% filter(busstops$subzone_name %in% input$sz_from)
+                }
+                , fname)
+        }
+    ) 
+    
+    # Download table "to"
+    output$downloadTo <- downloadHandler(
+        filename = function(){"destination.csv"}, 
+        content = function(fname){
+            write.csv(
+                if (input$radio=="PA"){
+                    busstops%>% filter(busstops$planning_area %in% input$pa_to)
+                }
+                else{
+                    busstops%>% filter(busstops$subzone_name %in% input$sz_to)
+                }
+                , fname)
+        }
+    )
     
     
-    ## Select the dominant flows (incoming flows criterion)
-    flowSel2 <- domflows(mat = myflows, w = colSums(myflows), k = 1)
+    # Download table
+    #output$downloadFrom <- downloadHandler(
+    #    filename = function(){"source.csv"}, 
+    #    content = function(fname){
+    #        write.csv(mytableFrom , fname)
+    #    }
+    #)
     
-    ## Combine selections
-    flowSel <- myflows * flowSel1 * flowSel2
+    # https://shiny.rstudio.com/articles/layout-guide.html
+    # https://stackoverflow.com/questions/44504759/shiny-r-download-the-result-of-a-table
     
-    ## Node weights
-    inflows <- data.frame(id = colnames(myflows), w = colSums(myflows))
     
-    ## OLD: Plot dominant flows map
-    output$polygon_jy <- renderImage({plot(mpsz, col = "#cceae7", border = NA)})
-    #output$flow
-    opar <- par(mar = c(0,0,2,0))
-    ## Ploting the points
-    # output$flowDom_jy <- leaflet() %>% sp::plot(mpsz, col = "#cceae7", border = NA) %>%
-    # 
-    # 
-    # plotMapDomFlows(mat = flowSel, spdf = mpbus, spdfid = "BUS_STOP_N", w = inflows, wid = "id",
-    #                 wvar = "w", wcex = 0.05, add = TRUE,
-    #                 legend.flows.pos = "topright",
-    #                 legend.flows.title = "Nb. of commuters") %>%
-    # title("Dominant Flows of Commuters") %>%
-    # mtext(text = "singapore bus,2020", side = 4, line = -1, adj = 0.01, cex = 0.8)   ############## not work with map separated
+   
+    ########################################################### Map FLow Jia Yi ######################################################################
     
+ 
     # Subzone Flow map
     ## plot of graph starts here
-    map_jy <- ggplot(lay) + map_gg2+ map_gg3 +# ggraph(lay) 
+    
+    
+    #########
+    ## Filter node and edges by the selected subzone name in aggregate filter
+    # node_SZ <- node_SZ %>% filter(name == dataInput_From_SZ())
+    # 
+    # node_SZ_reactive <- reactive({
+    #   node_SZ %>% 
+    #     filter(name == input$sz_to)
+    # })
+    #node_SZ_reactive()
+    
+    # Create Graph object starts here
+    
+    # qwerty <- reactive({
+    #   print(node_SZ_reactive())
+    #   print('asd')
+    #   abc_rb <- 'aaaaaa'
+    #   print(abc_rb)
+    #   abc_rb
+    # })
+    # 
+    # observe({
+    #   print(qwerty)
+    # })
+    # 
+    ##### Map Flow App depends on the selection output from AggregateFilter
+    dataInput_From_SZ <- reactive({ # unused at the moment
+      if (input$radio=="SZ"){
+        input$sz_from
+      }
+    })
+    
+    dataInput_To_SZ <- reactive({
+      if (input$radio=="SZ"){  # unused at the moment
+      input$sz_to
+      }
+    })
+    
+    #################
+    # 
+    # filtered_df <- reactive({
+    #   
+    #   res <- df %>% filter(current_grade >= input$current)
+    #   res <- res %>% filter(projected_grade >= input$projected)
+    #   res <- res %>% filter(age >= input$age[1] & age <= input$age[2])
+    #   res <- res %>% filter(ethnicity %in% input$ethnicity | is.null(input$ethnicity))
+    #   
+    #   if(input$previous == TRUE)
+    #     res <- res %>% filter(previous_sale == 1)
+    #   
+    #   if(input$warm == TRUE)
+    #     res <- res %>% filter(warm_lead == 1)
+    #   
+    #   res
+    #   
+    # })
+    output$map_jy <- renderPlot({
+    # filter based on "Aggregate Filter" tab
+    if (input$radio=="SZ"){
+      # filter the Edges with temporary variable "node2" and "node3"
+      node2 <- node_SZ %>% filter(name %in% dataInput_From_SZ() )
+      edge_id2_SZ <- edge_id2_SZ[which(edge_id2_SZ$from %in% node2$id) ,] 
+      
+      node3 <- node_SZ %>% filter(name %in% dataInput_To_SZ())
+      edge_id2_SZ <- edge_id2_SZ[which(edge_id2_SZ$to %in% node3$id) ,]
+      # filter nodes 
+      node_SZ <- node_SZ %>% filter(name %in% dataInput_From_SZ() | name %in% dataInput_To_SZ())
+    }
+
+   
+    #node_SZ_new <- node_SZ
+  
+    
+    g_SZ <- graph_from_data_frame(edge_id2_SZ, directed = TRUE, vertices = node_SZ)
+    edges_for_plot_SZ <- edge_id2_SZ %>% 
+      inner_join(node_SZ %>% select(id, x, y), by = c('from' = 'id')) %>%
+      inner_join(node_SZ %>% select(id, x, y), by = c('to' = 'id')) 
+    
+    print(edges_for_plot_SZ)
+    
+    node_SZ$weight = degree(g_SZ)
+    
+    maptheme <- theme(panel.grid = element_blank()) +
+      theme(axis.text = element_blank()) +
+      theme(axis.ticks = element_blank()) +
+      theme(axis.title = element_blank()) +
+      theme(legend.position = "bottom") +
+      theme(panel.grid = element_blank()) +
+      theme(panel.background = element_rect(fill = "#596673")) +
+      theme(plot.margin = unit(c(0, 0, 0.5, 0), 'cm'))
+    
+    node_pos_SZ <- node_SZ %>%
+      select(x, y)  # node positions must be called x, y
+    lay_SZ <- create_layout(g_SZ, 'manual',
+                            node.positions = node_pos_SZ)
+    
+    lay_SZ <- lay_SZ[ -c(1,2) ]
+    # add node degree for scaling the node sizes
+    lay_SZ$weight <- degree(g_SZ)
+    # We pass the layout lay and use ggraph's geoms geom_edge_arc and geom_node_point for plotting:
+    
+    # convert all columns to numeric
+    # https://stackoverflow.com/questions/19146354/batch-convert-columns-to-numeric-type
+    
+    edges_for_plot4_SZ <- edges_for_plot_SZ %>%
+      select(c('from','to','DAY_TYPE','weight','category','x.x','y.x','x.y','y.y')) %>%
+      rename(x = x.x) %>%
+      rename(y = y.x) %>%
+      rename(xend = x.y) %>%
+      rename(yend = y.y)
+    
+    # change columns from numerical to numeric then back to character (due to error msgs)
+    edges_for_plot4_SZ$x    <- as.numeric(as.character(edges_for_plot4_SZ$x))
+    edges_for_plot4_SZ$y    <- as.numeric(as.character(edges_for_plot4_SZ$y))
+    edges_for_plot4_SZ$xend <- as.numeric(as.character(edges_for_plot4_SZ$xend))
+    edges_for_plot4_SZ$yend <- as.numeric(as.character(edges_for_plot4_SZ$yend))
+    
+    lay_SZ$x <-  as.numeric(as.character(lay_SZ$x))
+    lay_SZ$y <-  as.numeric(as.character(lay_SZ$y))
+    
+    
+    
+    # create bins for colours
+    edges_for_plot4_SZ <- edges_for_plot4_SZ %>%
+      mutate(
+        bin = cut(weight, seq(0,max(edges_for_plot4_SZ$weight),2), na.rm=TRUE)) %>%
+      ungroup()
+    
+    
+    
+    
+    
+    
+    
+ 
+    
+    map_jy <- ggplot(lay_SZ) + map_gg2_SZ+ map_gg3_SZ +# ggraph(lay) 
         geom_edge_arc(aes(edge_width = weight,   # draw edges as arcs
                           circular = FALSE,show.legend = TRUE),
-                      data = edges_for_plot4, curvature = 0.33,
+                      data = edges_for_plot4_SZ, curvature = 0.33,
                       alpha = 0.5) +
-        scale_edge_width_continuous(range = c(0.5,50),             # scale for edge widths
+        scale_edge_width_continuous(range = c(0.5,5),             # scale for edge widths
                                     guide = FALSE) +
-        geom_node_point(aes(size = weight),show.legend = FALSE, shape = 21,            # draw node
-                        fill = "white", color = "black",
-                        stroke = 0.5) +
+        geom_node_point(aes(size = weight, color= as.factor(district)),show.legend = FALSE         # draw node
+        ) +
         scale_size_continuous(range = c(1, 10), guide = FALSE) +    # scale for node sizes
         geom_node_text(aes(label = name),show.legend = FALSE, repel = TRUE, size = 3,
                        color = "white", fontface = "bold") +
         maptheme
-    output$map_jy <- renderPlot({
-        plot(map_jy)
-        }, height = 600, width = 2000)
     
+        plot(map_jy)
+    }, height = 600, width = 2000)
+    
+    ################## HeatMap JY ####################################
+    # # Remove the matrix diagonal
+    # 
+    # # Selection of flows > 500
+    # flowSel1 <- firstflowsg(mat = myflows, method = "xfirst", k = 5000)
+    # # Selection of flows > 1000
+    # flowSel2 <- firstflowsg(mat = myflows, method = "xfirst", k = 10000)
+    # # table output where flowSel1 ( k <=5000))
+    # flowtable1 <- compmat(mat1 = myflows, mat2 = myflows * flowSel1, digits = 1)
+    # # table output where flowSel1 ( k <=10000))
+    # flowstable2 <- compmat(mat1 = myflows, mat2 = myflows * flowSel2, digits = 1)
+    # 
+    # output$flow_jy <- renderPrint({ 
+    #   {if(input$flow_input== 5000){flowtable1
+    #   }else if(input$flow_input == 10000){flowstable2
+    #   }
+    #   }
+    # }) # end of renderPrint Statistics
+    # 
+    # # Plot Flows diagram
+    # ## Remove the matrix diagonal
+    # diag(myflows) <- 0
+    # 
+    # ## Select flows that represent at least 20% of the sum of outgoing flows for 
+    # ## each urban area. ( can select other methods )
+    # flowSel1 <- firstflows(mat = myflows/rowSums(myflows)*100, method = "xfirst", 
+    #                        k = 20)
+    # 
+    # 
+    # ## Select the dominant flows (incoming flows criterion)
+    # flowSel2 <- domflows(mat = myflows, w = colSums(myflows), k = 1)
+    # 
+    # ## Combine selections
+    # flowSel <- myflows * flowSel1 * flowSel2
+    # 
+    # ## Node weights
+    # inflows <- data.frame(id = colnames(myflows), w = colSums(myflows))
+    # 
+    # ## OLD: Plot dominant flows map
+    # output$polygon_jy <- renderImage({plot(mpsz, col = "#cceae7", border = NA)})
+    # #output$flow
+    # opar <- par(mar = c(0,0,2,0))
+    # ## Ploting the points
+    # # output$flowDom_jy <- leaflet() %>% sp::plot(mpsz, col = "#cceae7", border = NA) %>%
+    # # 
+    # # 
+    # # plotMapDomFlows(mat = flowSel, spdf = mpbus, spdfid = "BUS_STOP_N", w = inflows, wid = "id",
+    # #                 wvar = "w", wcex = 0.05, add = TRUE,
+    # #                 legend.flows.pos = "topright",
+    # #                 legend.flows.title = "Nb. of commuters") %>%
+    # # title("Dominant Flows of Commuters") %>%
+    # # mtext(text = "singapore bus,2020", side = 4, line = -1, adj = 0.01, cex = 0.8)   ############## not work with map separated
+    # 
+    # 
+    # 
+    # 
+
+
+
     
     ############################################# Meng Yong ########################################################
     
@@ -83,10 +351,10 @@ server <- function(input, output) {
     
     
     ################################################ Yong Shan #####################################################
+
+
+
 }
-
-
-
 # https://shiny.rstudio.com/gallery/file-upload.htmls
 # https://cran.r-project.org/web/packages/ggraph/vignettes/tidygraph.html
 # https://rstudio.github.io/shinydashboard/structure.html#background-shiny-and-html
--- ProjRShiny_jiayi/ui.R
+++ ProjRShiny_jiayi/ui.R
@@ -10,10 +10,13 @@ ui <- dashboardPage(
     dashboardSidebar(width = 200,
                      sidebarMenu(id = 'sbm',
                                  menuItem('Home', tabName = 'Home', icon=icon("home")),
-                                 menuItem('Flow', tabName = 'Flow_Diagrams', icon = icon("bus")),
-                                 menuItem('Proportional_Symbol_Map', tabName = 'ProportionalSymbolMap_Diagrams', icon = icon("connectdevelop")),
+                                 menuItem('Flow', tabName = 'Flow_Diagrams', icon = icon("bus"),startExpanded= TRUE,
+                                    menuSubItem("Aggregation Filters", tabName = "Aggregation_Filters"),
+                                    menuSubItem("Flow Map", tabName = "Flow_Map"),
+                                    menuSubItem("Sankey Diagram", tabName = "Sankey_Diagram")),
+                                 menuItem('Proportional Symbol_Map', tabName = 'ProportionalSymbolMap_Diagrams', icon = icon("connectdevelop")),
                                  menuItem('Centrality', tabName = 'Centrality', icon = icon("globe")),
-                                 menuItem('GravityModel', tabName = 'Gravity_Model', icon = icon("chart-line")),
+                                 menuItem('Gravity Model', tabName = 'Gravity_Model', icon = icon("chart-line")),
                                  menuItem('Download', tabName = 'Download', icon = icon("download"))# tabs are here!
                                 )
                     ), # end of DashboardSiderBar
@@ -48,7 +51,85 @@ ui <- dashboardPage(
                     )
             ), #end of tabname "home" 
             
-            tabItem(tabName = 'Flow_Diagrams',
+            tabItem(tabName = 'Aggregation_Filters',
+                    fluidPage(
+
+                        titlePanel("Aggregation Filters"),
+                        fluidRow(
+
+                            sidebarLayout(
+                                
+                                    sidebarPanel(
+                                        helpText("Select passenger flow size"),
+                                        verbatimTextOutput('ex_out'),
+                                        # I() indicates it is raw JavaScript code that should be evaluated, instead
+                                        # of a normal character string
+                                        ## Select dropdown of either filter by Planning Area or Filter by Subzone
+                                        radioButtons("radio", h3("Choose type of filter"),
+                                                     choices = list("Planning Area" = 'PA', 
+                                                                    "Subzone" = 'SZ'),selected = 'PA'),
+                                        #switch view to PA
+                                        conditionalPanel( condition = "input.radio=='PA'", #input.radio == 'PA'
+                                            # if you select planning area, then show this
+                                            selectizeInput(
+                                                'pa_from', 'From Planning Area', choices = unique(busstops$planning_area) ,
+                                                multiple = TRUE,
+                                                options = list(maxItems = 10,
+                                                   placeholder = 'Please select one or more options below',
+                                                   onInitialize = I('function() { this.setValue(""); }')
+                                                )
+                                            ), #end of selectizeInput from Planning Area
+                                            
+                                            selectizeInput(
+                                                'pa_to', 'To Planning Area', choices = unique(busstops$planning_area) ,
+                                                multiple = TRUE,
+                                                options = list(maxItems = 10,
+                                                    placeholder = 'Please select one or more options below',
+                                                    onInitialize = I('function() { this.setValue(""); }')
+                                                )
+                                            )#end of selectizeInput to Planning Area
+                                        ), # end of conditionalPanel
+                                        
+                                        #switch view to SZ
+                                        conditionalPanel( condition = "input.radio=='SZ'", 
+                                                          # if you select subzone, then show this
+                                                          selectizeInput(
+                                                              'sz_from', 'From SubZone', choices = unique(busstops$subzone_name) ,
+                                                              multiple = TRUE,
+                                                              options = list(maxItems = 10,
+                                                                  placeholder = 'Please select one or more options below',
+                                                                  onInitialize = I('function() { this.setValue(""); }')
+                                                              )
+                                                          ), #end of selectizeInput from Planning Area
+                                                          
+                                                          selectizeInput(
+                                                              'sz_to', 'To SubZone', choices = "initialisation"  , #initialise
+                                                              #'sz_to', 'To SubZone', choices = unique(busstops$subzone_name) ,
+                                                              multiple = TRUE,
+                                                              options = list(maxItems=10,
+                                                                  placeholder = 'Please select one or more options below',
+                                                                  onInitialize = I('function() { this.setValue(""); }')
+                                                              )
+                                                          )#end of selectizeInput to Planning Area
+                                        ) # end of conditionalPanel
+                                        #submitButton("Apply changes")
+                                ) ,
+                               
+                                mainPanel(
+                                    tabsetPanel(
+                                        tabPanel("Source Busstops", withSpinner(DT::dataTableOutput("mytableFrom"), type=2),downloadButton('downloadFrom',"Download the table")), 
+                                        tabPanel("Destination Busstops", withSpinner(DT::dataTableOutput("mytableTo"), type=2),downloadButton('downloadTo',"Download the table"))
+                                    )# https://www.listendata.com/2019/07/add-loader-for-shiny-r.html
+                                    
+
+                                ) # end of mainPanel
+                            ) # end of sidebarlayout
+
+                        ) # end of fluidRow
+                    ) # end of fluidPage
+            ), #end of tabname "MainAggregate"
+            
+            tabItem(tabName = 'Flow_Map',
                     fluidPage(
                         
                         titlePanel("Flow Maps"),
@@ -60,14 +141,39 @@ ui <- dashboardPage(
                                     
                                     selectInput("flow_input", 
                                                 label = "flow k",
-                                                choices = c(5000, 
-                                                            10000
+                                                choices = c(20, 
+                                                            50,
+                                                            100,
+                                                            300,
+                                                            500,
+                                                            1000,
+                                                            1500,
+                                                            2000,
+                                                            3000,
+                                                            "more than 5000"
                                                             ),
-                                                selected = 10000),
+                                                selected = 100),
                                     
-                                    sliderInput("range", 
+                                    sliderInput("flowsize", 
                                                 label = "Range of interest:",
-                                                min = 0, max = 100, value = c(0, 100))
+                                                min = 0, max = 100000, value = c(0, 100)),
+                                    sliderInput("busstopNetworksize", 
+                                                label = "Range of interest:",
+                                                min = 0, max = 100, value = c(0, 100)),
+                                    selectInput("district", 
+                                                label = "flow k",
+                                                choices = c(20, 
+                                                            50,
+                                                            100,
+                                                            300,
+                                                            500,
+                                                            1000,
+                                                            1500,
+                                                            2000,
+                                                            3000,
+                                                            "more than 5000"
+                                                ),
+                                                selected = 100)
                                 ),
                                 column(
                                     mainPanel(
@@ -84,7 +190,7 @@ ui <- dashboardPage(
                             
                         ) # end of fluidRow
                     ) # end of fluidPage
-            ), #end of tabname "flow"  
+            ), #end of tabname "flowMap"  
             
             tabItem(tabName = 'ProportionalSymbolMap_Diagrams',
                     fluidPage(
